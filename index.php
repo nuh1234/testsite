2d29cbbac060cdd0b56b014b5b449b88a8f1857d
Nuh Mohamud <nuh.mohamud@gmail.com>
2017-11-05 20:22:23 -0800

    Fixing a ton of CSS bugs. Content is now centered based on percentages for a wider range of compatable devices

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <?php include('sendForm.php'); ?>
 <link rel="stylesheet" href="main.css" type="text/css">
-<div class="container">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,21 +10,19 @@
     <h1>testing file input build</h1>
       <p> Resume:<br>
       <input type="file" name="resumeFile", accept=".pdf, .doc" value="<?= $file ?>"><br>
-      <span class="underline"></span>
       <span class="error"><?= $file_error ?></span></p>
       <p>
-      <input class= "input" placeholder="First" type="text" name="firstname" value="<?= $firstname ?>"><br>
-      <span class="underline"></span>
-      <span class="error"><?= $firstname_error ?></span></p>
-      <p>
-      <input class = "input" placeholder="Last" type="text" name="lastname" value="<?= $lastname ?>"><br>
-      <span class="underline"></span>
-      <span class="error"><?= $lastname_error ?></span></p>
-      <p>
-      <input class= "input" placeholder="Your Email" type="text" name="email" value="<?= $email ?>">
-      <span class="underline"></span>
-      <span class="error"><?= $email_error ?></span></p>
-      <p><input type="submit" value="Submit">
-      <div class="success"><?= $success ?></div></p>
+      <div class="wrapper">
+        <input class= "input" placeholder="First" type="text" name="firstname" value="<?= $firstname ?>">
+        <span class="underline"></span>
+        <span class="error"><?= $firstname_error ?></span></p>
+        <p>
+        <input class = "input" placeholder="Last" type="text" name="lastname" value="<?= $lastname ?>">
+        <span class="error"><?= $lastname_error ?></span></p>
+        <p>
+        <input class= "input" placeholder="Your Email" type="text" name="email" value="<?= $email ?>">
+        <span class="error"><?= $email_error ?></span></p>
+        <p><input type="submit" value="Submit">
+        <div class="success"><?= $success ?></div></p>
+      </div>
   </form>
-</div>
