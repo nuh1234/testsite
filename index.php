f6f2fa8efd8041ea967580a04359eb0329caad66
Nuh Mohamud <nuh.mohamud@gmail.com>
2017-11-11 13:33:14 -0800

    Adding more to the appication requirements

--- index.php
+++ index.php
@@ -1,35 +1,48 @@
 
-<?php include('sendForm.php'); ?>
-<link rel="stylesheet" href="main.css" type="text/css">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Application Page</title>
-  </head>
-  <form id="contact" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-    <h1>testing file input build</h1>
-      <div class="wrapper">
-        <input class= "input" placeholder="First" type="text" name="firstname" value="<?= $firstname ?>">
-        <span class="underline"></span><br>
-        <span class="error"><?= $firstname_error ?></span></p>
-        <p>
-        <input class = "input" placeholder="Last" type="text" name="lastname" value="<?= $lastname ?>">
-        <span class="underline"></span><br>
-        <span class="error"><?= $lastname_error ?></span></p>
-        <p>
-        <input class= "input" placeholder="Phone ex: (555)-555-5555" type="text" name="phonenumber" value="<?= $phonenumber ?>">
-        <span class="underline"></span><br>
-        <span class="error"><?= $phonenumber_error ?></span></p>
-        <p>
-        <p>
-        <input class= "input" placeholder="Your Email" type="text" name="email" value="<?= $email ?>">
-        <span class="underline"></span><br>
-        <span class="error"><?= $email_error ?></span></p>
-        <p>
-          <input type="file" name="resumeFile", accept=".pdf, .doc" value="<?= $file ?>"><br>
-          <span class="error"><?= $file_error ?></span></p>
-          <input type="submit" value="Apply" id="submitHover">
-        <p>
-        <div class="success"><?= $success ?></div></p>
-      </div>
-  </form>
+
+<head>
+  <link rel="stylesheet" href="main.css" type="text/css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Application Page</title>
+</head>
+<form>
+  <h1>testing file input build</h1>
+  <div class="wrapper">
+    <input type="file" name="resumeFile", accept=".pdf, .doc"><br>
+    <div>
+      <input class= "input" placeholder="First" type="text" name="firstname">
+      <span class="underline"></span>
+      <input class = "input" placeholder="Last" type="text" name="lastname">
+      <span class="underline"></span>
+    </div>
+    <input class= "input" placeholder="Permanent Address" type="text" name="address">
+    <span class="underline"></span><br>
+    <input class= "input" placeholder="Email Address" type="text" name="email">
+    <span class="underline"></span><br>
+    <input class= "input" placeholder="Phone ex: (555)-555-5555" type="text" name="phonenumber">
+    <span class="underline"></span><br>
+    <input class= "input" placeholder="School Name" type="text" name="schoolname">
+    <span class="underline"></span><br>
+    <div>
+
+      <select>
+        <option value="CS">ComSci/Com Eng</option>
+        <option value="EE">Electrical Engineering</option>
+        <option value="HCI">Human Computer Interaction</option>
+        <option value="OTHER">Other</option>
+      </select>
+      <select>
+        <option value="CS">ComSci/Com Eng</option>
+        <option value="EE">Electrical Engineering</option>
+        <option value="HCI">Human Computer Interaction</option>
+        <option value="OTHER">Other</option>
+      </select>
+    </div>
+    <div>
+
+      <input type="submit" value="Apply" id="submitHover">
+    </div>
+  </p>
+</div>
+</form>
