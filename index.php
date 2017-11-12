5556b7a2d9f1bec9a59be28f03a9becbb77db4ee
Nuh Mohamud <nuh.mohamud@gmail.com>
2017-11-12 02:22:19 -0800

    Wrote over 15 custom Java Script functions for validation. Next step is serializing data for API request

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Application Page</title>
 </head>
-<form name = "Application" onsubmit="return checkMajorTwo()">
+<form name = "Application" onsubmit="return checkFileUploaded()">
   <h1>file input build</h1>
   <div class="wrapper">
-    Upload your resume <input type="file" name="resumeFile", accept=".pdf, .doc"><br>
+    Upload your resume <input type="file" id = "file" name="resumeFile", accept = ".pdf, .doc"><br>
     <div>
       <input class= "input" placeholder="First" type="text" id = "firstname">
       <span class="underline"></span>
@@ -20,9 +20,9 @@
 
     <input class= "input" placeholder="Permanent Address" type="text" id = "address">
     <span class="underline"></span><br>
-    <input class= "input" placeholder="Email Address" type="text" id ="email">
+    <input class= "input" placeholder="Email Address" type="text" id = "email">
     <span class="underline"></span><br>
-    <input class= "input" placeholder="Phone ex: (555)-555-5555" type="text" id ="phonenumber">
+    <input class= "input" placeholder="Phone ex: (555)-555-5555" type = "text" id ="phonenumber">
     <span class="underline"></span><br>
     <input class= "input" placeholder="School Name" type="text" id ="schoolname">
     <span class="underline"></span><br>
@@ -31,7 +31,7 @@
       <h4>Major/s or Intended Major/s</h4>
 
       <label name="PrimaryMajor"> Primary</label>
-      <select name="selectMajor1" id = "primarymajor" onchange="showOtherBox(this.options[this.selectedIndex].value)">
+      <select name="selectMajor1" id = "primarymajor" onchange = "showOtherBox(this.options[this.selectedIndex].value)">
         <option value="CS">Computer: Science or Engineering</option>
         <option value="EE">Electrical Engineering</option>
         <option value="HCI">Human Computer Interaction</option>
@@ -40,7 +40,7 @@
       <div id ="OtherBox"></div>
 
       <label name="SecondaryMajor"> Secondary</label>
-      <select name="selectMajor2" id = "secondarymajor" onchange="showOtherBox2(this.options[this.selectedIndex].value)">
+      <select name="selectMajor2" id = "secondarymajor" onchange = "showOtherBox2(this.options[this.selectedIndex].value)">
         <option value="CS">Computer: Science or Engineering</option>
         <option value="EE">Electrical Engineering</option>
         <option value="HCI">Human Computer Interaction</option>
@@ -56,25 +56,26 @@
     <input type="radio" name = "Year" id = "YearInSchool" value="Sophomore"> Sophomore <br>
 
     <h5>Have you completed Introduction to Computer Science or an equivalent course?</h5>
-    <input type="radio" name = "CompletedCSCourse" value="Yes" id = "csYes" onclick="showCourseTextBox()"> Yes
-    <input type="radio" name = "CompletedCSCourse" value="No" id = "csNo" onclick="showCourseTextBox()"> No
+    <input type="radio" name = "CompletedCSCourse" value="true" id = "csYes" onclick = "showCourseTextBox()"> Yes
+    <input type="radio" name = "CompletedCSCourse" value="false" id = "csNo" onclick = "showCourseTextBox()"> No
     <div id ="CompletedCourseBox"></div>
 
     <h5>Have you completed College Algebra or an equivalent course?</h5>
-    <input type="radio" name = "Math" id = "CompletedMathCourse" value="Yes"> Yes
-    <input type="radio" name = "Math" id = "CompletedMathCourse" value="No"> No
+    <input type="radio" name = "Math" id = "CompletedMathCourse" value="true"> Yes
+    <input type="radio" name = "Math" id = "CompletedMathCourse" value="false"> No
 
 
     <h4>Optional Demographics:</h4>
 
     <h5>Ethnic Background (Check as many as apply)</h5>
-    <input type="checkbox" name="AfricanAmerican" value="AfricanAmerican"> Black or African American
-    <input type="checkbox" name="AsianAmerican" value="AsianAmerican"> Asian American
-    <input type="checkbox" name="White" value="White"> White
-    <input type="checkbox" name="LatinoHispanic" value= "LatinoHispanic"> Hispanic, Latino or Spanish origin <br>
-    <input type="checkbox" name="Native" value="Native"> Native American/Alaskan Native
-    <input type="checkbox" name="PacificIslander" value="PacificIslander"> Pacific Islander
-    <input type="checkbox" name="Other" value="Other"> Other <br>
+    <input class = "raceCheckBox" type="checkbox" name="AfricanAmerican" value="AfricanAmerican"> Black or African American
+    <input class = "raceCheckBox" type="checkbox" name="AsianAmerican" value="AsianAmerican"> Asian American
+    <input class = "raceCheckBox" type="checkbox" name="White" value="White"> White
+    <input class = "raceCheckBox" type="checkbox" name="LatinoHispanic" value= "LatinoHispanic"> Hispanic, Latino or Spanish origin <br>
+    <input class = "raceCheckBox" type="checkbox" name="Native" value="Native"> Native American/Alaskan Native
+    <input class = "raceCheckBox" type="checkbox" name="PacificIslander" value="PacificIslander"> Pacific Islander
+    <input class = "raceCheckBox" type="checkbox" name="Other" value="Other" id = "otherCheckBox" onclick="showOtherBox3()"> Other <br>
+    <div id ="OtherBox3"></div>
 
     <div>
       <h5>Gender Identity</h5>
--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-function showOtherBox(name) {
-  if(name == 'Other') {
-    document.getElementById('OtherBox').innerHTML = 'Other: <input type="text" name="other" id = "majorOne" />';
-  } else {
-    document.getElementById('OtherBox').innerHTML = '';
-  }
-}
-function showOtherBox2(name) {
-  if(name == 'Other') {
-    document.getElementById('OtherBox2').innerHTML = 'Other 2: <input type="text" name="other2" id = "majorTwo" />';
-  } else {
-    document.getElementById('OtherBox2').innerHTML = '';
-  }
-}
-function showCourseTextBox() {
-  var value = document.getElementById('csYes');
-  if(value.checked) {
-    document.getElementById('CompletedCourseBox').innerHTML = 'Please list course name: <input type="text" name="courseName" id = "OtherCourse"/>';
-  } else {
-    document.getElementById('CompletedCourseBox').innerHTML = '';
-  }
-}
+
 function checkFirstName() {
   var firstName = document.getElementById('firstname').value;
   if(firstName == "" || !/^[a-zA-Z ]*$/.test(firstName) || !firstName.replace(/\s/g, '').length) {
@@ -28,6 +7,7 @@ function checkFirstName() {
   }
   return firstName;
 }
+
 function checkLastName() {
   var lastName = document.getElementById('lastname').value;
   if(lastName == "" || !/^[a-zA-Z ]*$/.test(lastName) || !lastName.replace(/\s/g, '').length) {
@@ -36,6 +16,7 @@ function checkLastName() {
   }
   return lastName;
 }
+
 function checkAddress() {
   var address = document.getElementById('address').value;
   if(address == "" || !address.replace(/\s/g, '').length) {
@@ -44,6 +25,7 @@ function checkAddress() {
   }
   return address;
 }
+
 function checkEmail() {
   var email = document.getElementById('email').value;
   if(email == "" || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
@@ -52,6 +34,7 @@ function checkEmail() {
   }
   return email;
 }
+
 function checkPhone() {
   var phoneNumber = document.getElementById('phonenumber').value;
   if(phoneNumber == "" || !/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/.test(phoneNumber)) {
@@ -60,6 +43,7 @@ function checkPhone() {
   }
   return phoneNumber;
 }
+
 function checkSchool() {
   var school = document.getElementById('schoolname').value;
   if(school == "" || !school.replace(/\s/g, '').length) {
@@ -69,11 +53,11 @@ function checkSchool() {
   return school;
 }
 
-function checkMajorOne() {
+function checkMajorOne() { //If the major is other, I also check the other input box
   var major = document.getElementById('primarymajor').value;
   if(major == "Other") {
     var otherInput = document.getElementById('majorOne').value;
-    if(otherInput == "" || !otherInput.replace(/\s/g, '').length) { //This checks if an input is all spaces
+    if(otherInput == "" || !otherInput.replace(/\s/g, '').length) {
       alert("Please finish");
       return false;
     }
@@ -81,6 +65,7 @@ function checkMajorOne() {
   }
   return major;
 }
+
 function checkMajorTwo() {
   var major = document.getElementById('secondarymajor').value;
   if(major == "Other") {
@@ -92,4 +77,122 @@ function checkMajorTwo() {
     return otherInput;
   }
   return major;
+}
+
+function checkYearInSchool() { //radio values have to be extracted from an array :/
+  var year = document.getElementsByName('Year');
+  if(year[0].checked) {
+    alert(year[0].value);
+    return year[0].value;
+  }else if (year[1].checked) {
+    alert(year[1].value);
+    return year[1].value;
+  }
+  alert("Select a year");
+  return false;
+}
+
+function checkCSCourse() { //If they've completed a cs course, I also check the text box
+var csRadio = document.getElementsByName('CompletedCSCourse');
+if(csRadio[0].checked) {
+  var courseName = document.getElementById('OtherCourse').value;
+  if (courseName == "" || !courseName.replace(/\s/g, '').length) {
+    alert("Please enter CS course name");
+    return false;
+  }
+  alert(courseName);
+  return courseName;
+}else if (csRadio[1].checked) {
+  alert(csRadio[1].value);
+  return csRadio[1].value;
+}
+alert("Select Yes or No for CS course");
+return false;
+}
+
+function checkMathCourse() {
+  var mathRadio = document.getElementsByName('Math');
+  if(mathRadio[0].checked) {
+    alert(mathRadio[0].value);
+    return mathRadio[0].value;
+  }else if (mathRadio[1].checked) {
+    alert(mathRadio[1].value);
+    return mathRadio[1].value;
+  }
+  alert("Select a math course");
+  return false;
+}
+
+function getGender() {
+  var genderRadio = document.getElementsByName('Gender');
+  if(genderRadio[0].checked) {
+    return genderRadio[0].value;
+  }else if(genderRadio[1].checked) {
+    return genderRadio[1].value;
+  }else if (genderRadio[2].checked) {
+    return genderRadio[2].value;
+  }
+  return "";
+}
+
+function getRace() {
+  var race = [];
+  var boxes = document.getElementsByClassName('raceCheckBox');
+  for (var i = 0; boxes[i]; i++) {
+    if (boxes[i].checked) {
+      if(boxes[i].value == "Other") {
+        var other = document.getElementById('otherRace').value;
+        if(other == "" || !other.replace(/\s/g, '').length) {
+          alert("Please enter text");
+          return false;
+        }
+        race.push(other);
+      } else {
+        race.push(boxes[i].value);
+      }
+    }
+  }
+  alert(race.toString());
+  //Api might want all strings, in that case return race.toString();
+  return race;
+}
+
+function checkFileUploaded() {
+  var file = document.getElementById('file');
+  if(!file.files.length) {
+    alert("Please attach file");
+    return false;
+  }
+  return file;
+}
+//These 4 functions below make text boxes appear
+function showOtherBox(name) {
+  if(name == 'Other') {
+    document.getElementById('OtherBox').innerHTML = 'Other: <input type="text" name="other" id = "majorOne" />';
+  } else {
+    document.getElementById('OtherBox').innerHTML = '';
+  }
+}
+function showOtherBox2(name) {
+  if(name == 'Other') {
+    document.getElementById('OtherBox2').innerHTML = 'Other: <input type="text" name="other2" id = "majorTwo" />';
+  } else {
+    document.getElementById('OtherBox2').innerHTML = '';
+  }
+}
+function showOtherBox3() {
+  var otherCheckBox = document.getElementById('otherCheckBox');
+  if(otherCheckBox.checked) {
+    document.getElementById('OtherBox3').innerHTML = 'Other: <input type="text" name="otherRace" id = "otherRace" />';
+  } else {
+    document.getElementById('OtherBox3').innerHTML = '';
+  }
+}
+function showCourseTextBox() {
+  var value = document.getElementById('csYes');
+  if(value.checked) {
+    document.getElementById('CompletedCourseBox').innerHTML = 'Please list course name: <input type="text" name="courseName" id = "OtherCourse"/>';
+  } else {
+    document.getElementById('CompletedCourseBox').innerHTML = '';
+  }
 }
